a0e6b1353a53ce97461506e87e64ccf4a03df905
jeenie <jeenie828@gmail.com>
2018-10-20 13:56:32 +0900

    add_index_admin

---
+++ src/main/java/net/skhu/dto/Department.java
@@ -0,0 +1,22 @@
+package net.skhu.dto;
+
+public class Department {
+	int id;
+	int departmentName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(int departmentName) {
+		this.departmentName = departmentName;
+	}
+}
