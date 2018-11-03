b54d93ec33054e1707abdcce3794275942fc55f7
jihyuno301 <joh755807@gmail.com>
2018-11-03 19:40:24 +0900

    교수 추가/삭제 완료

--- src/main/java/net/skhu/controller/ProfessorController.java
+++ src/main/java/net/skhu/controller/ProfessorController.java
@@ -13,43 +13,64 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import net.skhu.dto.Department;
 import net.skhu.dto.Professor;
+import net.skhu.dto.Student;
 import net.skhu.mapper.DepartmentMapper;
 import net.skhu.mapper.ProfessorMapper;
 
 @Controller
 @RequestMapping("/user")
 public class ProfessorController {
+
 	@Autowired
 	ProfessorMapper professorMapper;
 	@Autowired
 	DepartmentMapper departmentMapper;
 
-	@RequestMapping("professorInquiry")
-	public String list(Model model) {
-		List<Professor> professors = professorMapper.findAll();
+	@RequestMapping(value="professorInquiry", method=RequestMethod.GET) 
+	public String professorInquiry(Model model) {
+		List<Professor> professors = professorMapper.findAll(); 
 		model.addAttribute("professors", professors);
-		return "graduation/professorInquiry";
+		return "user/professorInquiry";
 	}
+	
+	@RequestMapping(value="professorSearch", method=RequestMethod.GET)
+	public String professorSearch(Model model,
+			@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "id", required = false) Integer id,
+			@RequestParam(value = "departmentName", required = false) String departmentName) {
 
-	// 검색 조회
-	@RequestMapping(value = "professorInquiry", method = RequestMethod.GET)
-	public String professorInquiry(Model model,
-			@RequestParam(value = "name", required = false, defaultValue = "") String name,
-			@RequestParam(value = "id", required = false, defaultValue = "") Integer id,
-			@RequestParam(value = "departmentName", required = false, defaultValue = "") String departmentName) {
 		List<Professor> professors = professorMapper.findByInquiry(name, id, departmentName);
 		model.addAttribute("name", name);
 		model.addAttribute("id", id);
+		model.addAttribute("departmentName", departmentName);
 		model.addAttribute("professors", professors);
-		return "graduation/professorInquiry";
+
+		return "user/professorInquiry";
+	}
+
+	@RequestMapping(value = "professorAdd", method = RequestMethod.GET)
+	public String professorAdd(Model model) {
+		Professor professor = new Professor();
+		List<Department> departments = departmentMapper.findAll();
+		model.addAttribute("professor", professor);
+		model.addAttribute("departments", departments);
+		return "user/professorAdd";
 	}
 
 	@RequestMapping(value = "professorAdd", method = RequestMethod.POST)
-	@ResponseBody
-	public Professor professorAdd(@RequestBody Professor professor) {
-		// professor 추가작업
+	public String professorAdd(Model model, Professor professor) {
+		int r1 = (int) (Math.random() * 1000);
+		String r2 = Integer.toString(r1);
+		professor.setEmail("default" + r2 + "@naver.com");// professor 테이블에서 not null처리가 되어있어서 임의로 값 대입
+		professor.setPassword(r2); // professor 테이블에서 not null처리가 되어있어서 임의로 값 대입
 		professorMapper.insert(professor);
-		return professor;
+		return "redirect:professorInquiry";
+	}
+
+	@RequestMapping("professorDelete")
+	public String professorDelete(@RequestParam("id") int id) {
+		professorMapper.delete(id);
+		return "redirect:professorInquiry";
 	}
 
 }
---
+++ src/main/webapp/res/js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+	
+  $("[data-url]").click(function() {
+    var url = $(this).attr("data-url");
+    location.href = url;
+  })
+
+  $("[data-confirm-delete]").click(function() {
+    return confirm("삭제하시겠습니까?");
+  })
+  
+  $("[data-confirm-professorAdd]".click(function() {
+	  return confirm('해당교수의 정보를 추가하시겠습니까?');
+  }))
+
+})
